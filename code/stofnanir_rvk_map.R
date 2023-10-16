6e754e1485cdd585642212ae095d5343146449f0
annaeva99 <annaeva99@gmail.com>
2023-10-16 16:26:22 +0000

    smá breyting

--- code/stofnanir_rvk_map.R
+++ code/stofnanir_rvk_map.R
@@ -9,13 +9,3 @@ leaflet(hnit_rvk) %>% addTiles() %>%
 
 
 hnit_rvk %>% select(long, lat) %>% as.matrix()
-
-ans <- as.data.frame(distm(hnit_rvk %>% select(long, lat) %>% as.matrix(), hnit_rvk %>% select(long, lat) %>% as.matrix(), fun=distHaversine)) 
-
-desired <- ans %>%
-  gather(pos1, distance) %>%
-  mutate(pos2 = rep(hnit_rvk$Stofnun, nrow(hnit_rvk))) %>%
-  filter(pos1!=pos2) %>%
-  select(pos1, pos2, distance)
-
-distHaversine(hnit_rvk %>% select(long, lat) %>% as.matrix())
