7ff3b1b93d94cb279b55c0ac9150803323987c78
annaeva99 <annaeva99@gmail.com>
2023-10-17 17:30:45 +0000

    Einfalt shiny app.

--- code/stofnanir_rvk_map.R
+++ code/stofnanir_rvk_map.R
@@ -1,12 +1,59 @@
-pacman::p_load( data.table, tidyr, gmapsdistance, tidyverse, ggmap, osmdata, maps, leaflet, widgetframe, sf, shiny, geosphere)
+pacman::p_load(arrow, data.table, tidyr, tidyverse, leaflet, sf)
 
-hnit_rvk <- fread("data/stofnanir_rvk_hnit.csv") %>% as_tibble()
+hnit_rvk <- fread("data/stofnanir_rvk_hnit.csv") %>% as_tibble() %>% drop_na(Stofnun)
+opnirreikningar_rvk <- read_parquet("data/opnirreikningar_rvk_hnit.parquet")
 
-leaflet(hnit_rvk) %>% addTiles() %>%
-  addCircleMarkers(lng = ~E_HNIT_WGS84, lat = ~N_HNIT_WGS84, 
-                   popup = ~Stofnun)
+hnit_opnirreikningar <- st_as_sf(opnirreikningar_rvk, coords = c("long","lat"), remove = FALSE)
 
+coords <- st_coordinates(hnit_opnirreikningar)
 
+# 
+# leaflet(hnit_opnirreikningar) %>% addTiles() %>%
+#   addCircleMarkers(lng = ~long, lat = ~lat, 
+#                    popup = ~kaupandi)
 
-hnit_rvk %>% select(long, lat) %>% as.matrix()
+## Shiny app sem sýnir á korti kaupendur fyrir hvern birgi
 
+library(shiny)
+library(dplyr)
+library(RColorBrewer)
+
+ui <- fluidPage(
+  titlePanel("Buyer Map"),
+  sidebarLayout(
+    sidebarPanel(
+      # Add input for selecting a company
+      selectInput("birgi", "Select Company", choices = unique(opnirreikningar_rvk$birgi)),
+      # You can add more input widgets here if needed
+    ),
+    mainPanel(
+      # Add the Leaflet map output
+      leafletOutput("map")
+    )
+  )
+)
+
+server <- function(input, output) {
+  # Define a reactive expression to filter data based on selected company
+  filtered_data <- reactive({
+    df_filtered <- subset(opnirreikningar_rvk, birgi == input$birgi)
+    df_aggregated <- df_filtered %>%
+      group_by(kaupandi, long, lat) %>%
+      summarize(total_spent = sum(upphaed_linu))
+    df_aggregated
+  })
+  
+  # Create the Leaflet map
+  output$map <- renderLeaflet({
+    leaflet() %>%
+      addTiles() %>%
+      addCircleMarkers(data = filtered_data(),
+                       ~long, ~lat,
+                       label = ~paste(kaupandi, "Spent:", total_spent),
+                       radius = 8,
+                       fillOpacity = 0.7, 
+                       color = ~colorNumeric("Spectral", domain = filtered_data()$total_spent)(total_spent))
+  })
+}
+
+shinyApp(ui, server)
